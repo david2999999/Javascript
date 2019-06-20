7397c56fb3c1102beef982d613487b53bb706157
David <djiang@smlny.com>
2019-06-20 07:40:24 -0400

    merging

--- Professional Javascript for Web Developers PDF/Chapter 6 - Object Oriented Programming/Inheritance/Combination Inheritance/Combination.js
+++ Professional Javascript for Web Developers PDF/Chapter 6 - Object Oriented Programming/Inheritance/Combination Inheritance/Combination.js
@@ -5,7 +5,7 @@
 // prototype and allows each instance to have its own properties. Consider the following:
 function SuperType(name){
     this.name = name;
-    this.colors = ['red', 'blue', 'green'];
+    this.colors = [“red”, “blue”, “green”];
 }
 SuperType.prototype.sayName = function(){
     alert(this.name);
@@ -21,13 +21,13 @@ SubType.prototype.sayAge = function(){
     alert(this.age);
 };
 
-var instance1 = new SubType('Nicholas', 29);
-instance1.colors.push('black');
-alert(instance1.colors); //'red,blue,green,black'
-instance1.sayName(); //'Nicholas';
+var instance1 = new SubType(“Nicholas”, 29);
+instance1.colors.push(“black”);
+alert(instance1.colors); //”red,blue,green,black”
+instance1.sayName(); //”Nicholas”;
 instance1.sayAge(); //29
 
-var instance2 = new SubType('Greg', 27);
-alert(instance2.colors); //'red,blue,green'
-instance2.sayName(); //'Greg';
+var instance2 = new SubType(“Greg”, 27);
+alert(instance2.colors); //”red,blue,green”
+instance2.sayName(); //”Greg”;
 instance2.sayAge(); //27
