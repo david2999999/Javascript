1d38e7c37393b5234efa4dc8a9d545c62dba23a6
david2999999 <djiang86@binghamton.edu>
2019-06-20 09:20:12 -0400

    removing side effects of closure functions and variables

--- Professional Javascript for Web Developers PDF/Chapter 7 - Function Expressions/Closure/ClosureAndVariables.js
+++ Professional Javascript for Web Developers PDF/Chapter 7 - Function Expressions/Closure/ClosureAndVariables.js
@@ -14,3 +14,23 @@ function createFunctions(){
 // object in its scope chain, they are all referring to the same variable, i. When createFunctions()
 // finishes running, the value of i is 10, and since every function references the same variable object in
 // which i exists, the value of i inside each function is 10
+
+function createFunctions(){
+    var result = new Array();
+    for (var i=0; i < 10; i++){
+        result[i] = function(num){
+            return function(){
+                return num;
+            };
+        }(i);
+    }
+    return result;
+}
+
+//With this version of createFunctions(), each function returns a different number. Instead of
+// assigning a closure directly into the array, an anonymous function is defi ned and called immediately.
+// The anonymous function has one argument, num, which is the number that the result function
+// should return. The variable i is passed in as an argument to the anonymous function. Since function
+// arguments are passed by value, the current value of i is copied into the argument num. Inside the
+// anonymous function, a closure that accesses num is created and returned. Now each function in the
+// result array has its own copy of num and thus can return separate numbers.
