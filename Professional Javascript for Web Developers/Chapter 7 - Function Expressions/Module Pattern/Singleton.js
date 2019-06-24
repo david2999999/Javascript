cecf0194131c4af93c9d5d6d6ae0d5184afa0916
david2999999 <djiang86@binghamton.edu>
2019-06-24 07:02:49 -0400

    singleton

---
+++ Professional Javascript for Web Developers/Chapter 7 - Function Expressions/Module Pattern/Singleton.js
@@ -0,0 +1,9 @@
+// Singletons are objects of
+// which there will only ever be one instance. Traditionally, singletons are created in JavaScript using
+// object literal notation, as shown in the following example:
+var singleton = {
+    name : value,
+    method : function () {
+        //method code here
+    }
+};
