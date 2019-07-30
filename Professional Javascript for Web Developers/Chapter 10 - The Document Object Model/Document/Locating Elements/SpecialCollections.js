8322f5d003b3a82603313d9e58e200b2eec6d2f1
david2999999 <djiang86@binghamton.edu>
2019-07-30 07:41:58 -0400

    other special collections

---
+++ Professional Javascript for Web Developers/Chapter 10 - The Document Object Model/Document/Locating Elements/SpecialCollections.js
@@ -0,0 +1,12 @@
+// document.anchors — Contains all <a> elements with a name attribute in the document.
+
+// document.applets — Contains all <applet> elements in the document. This collection is
+// deprecated, because the <applet> element is no longer recommended for use.
+
+// document.forms — Contains all <form> elements in the document. The same as document
+// .getElementsByTagName(“form”).
+
+// document.images — Contains all <img> elements in the document. The same as document
+// .getElementsByTagName(“img”).
+
+// document.links — Contains all <a> elements with an href attribute in the document.
