55af9d743071005127fc3e76894b24d939a6b730
david2999999 <djiang86@binghamton.edu>
2019-07-31 08:32:52 -0400

    splitting text node

---
+++ Professional Javascript for Web Developers/Chapter 10 - The Document Object Model/Document/Elements/SplittingTextNode.js
@@ -0,0 +1,16 @@
+// The Text type has a method that does the opposite of normalize(): the splitText() method splits
+// a text node into two text nodes, separating the nodeValue at a given offset. The original text node
+// contains the text up to the specifi ed offset, and the new text node contains the rest of the text. The
+// method returns the new text node, which has the same parentNode as the original.
+var element = document.createElement('div');
+element.className = 'message';
+
+var textNode = document.createTextNode('Hello world!');
+element.appendChild(textNode);
+
+document.body.appendChild(element);
+
+var newNode = element.fi rstChild.splitText(5);
+alert(element.fi rstChild.nodeValue); //'Hello'
+alert(newNode.nodeValue); //' world!'
+alert(element.childNodes.length); //2
