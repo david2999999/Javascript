8814c12c7a51771a1da37f8b14f5f6237fa20a81
David <djiang86@binghamton.edu>
2019-08-02 08:49:42 -0400

    query selector method

---
+++ Professional Javascript for Web Developers/Chapter 11 - DOM Extensions/Selector API/QuerySelector.js
@@ -0,0 +1,14 @@
+// The querySelector() method accepts a CSS query and returns the fi rst descendant element that
+// matches the pattern or null if there is no matching element.
+
+//get the body element
+var body = document.querySelector('body');
+
+//get the element with the ID 'myDiv'
+var myDiv = document.querySelector('#myDiv');
+
+//get first element with a class of 'selected'
+var selected = document.querySelector('.selected');
+
+//get first image with class of 'button'
+var img = document.body.querySelector('img.button');
