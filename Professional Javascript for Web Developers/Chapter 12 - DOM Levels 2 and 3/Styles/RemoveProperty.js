96a6af6d3aa31d8fbb1800d83e0d6da10fe17075
David <djiang86@binghamton.edu>
2019-08-09 08:22:50 -0400

    remove property

---
+++ Professional Javascript for Web Developers/Chapter 12 - DOM Levels 2 and 3/Styles/RemoveProperty.js
@@ -0,0 +1,5 @@
+// The removeProperty() method is used to remove a specifi c CSS property from the element’s
+// styling. Removing a property using this method means that any default styling for that property
+// (cascading from other style sheets) will be applied. For instance, to remove a border property that
+// was set in the style attribute,
+myDiv.style.removeProperty('border');
