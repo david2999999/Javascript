ecebfa43e70ead5c4567e01c973bdef7e755439a
David <djiang86@binghamton.edu>
2019-08-13 07:52:50 -0400

    insert node into DOM range

---
+++ Professional Javascript for Web Developers/Chapter 12 - DOM Levels 2 and 3/Range/Interacting with DOM Range Content/InsertNode.js
@@ -0,0 +1,13 @@
+var p1 = document.getElementById('p1'),
+helloNode = p1.firstChild.firstChild,
+    worldNode = p1.lastChild,
+    range = document.createRange();
+
+range.setStart(helloNode, 2);
+range.setEnd(worldNode, 3);
+
+var span = document.createElement('span');
+span.style.color = 'red';
+
+span.appendChild(document.createTextNode('Inserted text'));
+range.insertNode(span);
