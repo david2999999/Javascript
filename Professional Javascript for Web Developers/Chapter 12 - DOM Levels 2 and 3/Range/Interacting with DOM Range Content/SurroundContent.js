5af764ce11c06dc04220552391669ac18087deed
David <djiang86@binghamton.edu>
2019-08-13 08:02:22 -0400

    surround content in DOM range

---
+++ Professional Javascript for Web Developers/Chapter 12 - DOM Levels 2 and 3/Range/Interacting with DOM Range Content/SurroundContent.js
@@ -0,0 +1,10 @@
+var p1 = document.getElementById('p1'),
+helloNode = p1.firstChild.firstChild,
+    worldNode = p1.lastChild,
+    range = document.createRange();
+
+range.selectNode(helloNode);
+
+var span = document.createElement('span');
+span.style.backgroundColor = 'yellow';
+range.surroundContents(span);
