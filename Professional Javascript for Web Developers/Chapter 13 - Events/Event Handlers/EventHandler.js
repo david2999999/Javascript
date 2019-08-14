b4908bcc3fafd6a4c96cf73bbdd3e998f5e15774
David <djiang86@binghamton.edu>
2019-08-14 08:02:34 -0400

    event handling in HTML

---
+++ Professional Javascript for Web Developers/Chapter 13 - Events/Event Handlers/EventHandler.js
@@ -0,0 +1,12 @@
+// <script type=”text/javascript”>
+    // function showMessage(){
+    //     alert(“Hello world!”);
+    // }
+// </script>
+//
+// <input type=”button” value=”Click Me” onclick=”showMessage()” />
+
+
+// In this code, the button calls showMessage() when it is clicked. The showMessage() function is
+// defi ned in a separate <script> element and could also be included in an external fi le. Code executing
+// as an event handler has access to everything in the global scope.
