6c78391e0467d28f708a53e90899db33f47bfb0a
David <djiang86@binghamton.edu>
2019-08-19 07:35:14 -0400

    current target with event object

--- Professional Javascript for Web Developers/Chapter 13 - Events/The Event Object/DOMEventObject.js
+++ Professional Javascript for Web Developers/Chapter 13 - Events/The Event Object/DOMEventObject.js
@@ -6,3 +6,16 @@ btn.onclick = function(event){
 btn.addEventListener('click', function(event){
     alert(event.type); //'click'
 }, false);
+
+
+var btn = document.getElementById('myBtn');
+btn.onclick = function(event){
+    alert(event.currentTarget === this); //true
+    alert(event.target === this); //true
+};
+
+document.body.onclick = function(event){
+    alert(event.currentTarget === document.body); //true
+    alert(this === document.body); //true
+    alert(event.target === document.getElementById('myBtn')); //true
+};
