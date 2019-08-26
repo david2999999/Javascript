fb3780c0149828a0e28aa524df3c515f3549a951
David <djiang86@binghamton.edu>
2019-08-26 08:21:17 -0400

    disabling validation

---
+++ Professional Javascript for Web Developers/Chapter 14 - Scripting Forms/HTML5 Constraint Validation API/DisablingValidation.js
@@ -0,0 +1,14 @@
+// <form method=”post” action=”signup.php” novalidate>
+    // <!-- form elements here -->
+// </form>
+
+document.forms[0].noValidate = true; //turn off validation
+
+// If there are multiple submit buttons in a form, you can specify that the form not validate when a
+// particular submit button is used by adding the formnovalidate attribute to the button itself:
+
+// <form method=”post” action=”foo.php”>
+    // <!-- form elements here -->
+    // <input type=”submit” value=”Regular Submit”>
+    // <input type=”submit” formnovalidate name=”btnNoValidate” value=”Non-validating Submit”>
+// </form>
