ed87694abae22b7bba9deff549a16da5223468ef
David Jiang <djiang86@binghamton.edu>
2022-04-28 10:18:33 -0400

    introduction to strings

---
+++ Modern Javascript BootCamp/Section 3 - Variables and Flow Control/String and Variables/strings.js
@@ -0,0 +1,15 @@
+let name = 'Andrew Mead';
+
+console.log(name);
+
+let firstName = 'Andrew';
+let lastName = 'Mead';
+let fullName = firstName + ' ' + lastName;
+
+console.log(fullName);
+
+let city = 'Philadelphia';
+let country = 'United States';
+let location = city + ', ' + country;
+
+console.log(location);
