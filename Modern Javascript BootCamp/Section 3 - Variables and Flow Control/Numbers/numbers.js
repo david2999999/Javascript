972595780d3a0d6d7c7798cf890bf30703a8cc01
David Jiang <djiang86@binghamton.edu>
2022-04-29 11:12:07 -0400

    number operations

---
+++ Modern Javascript BootCamp/Section 3 - Variables and Flow Control/Numbers/numbers.js
@@ -0,0 +1,15 @@
+let x = 11;
+let num = (x + 1) * 2;
+
+console.log(num);
+
+let age = 26;
+let dogYears = (age + 1) / 7;
+
+console.log(dogYears);
+
+let studentScore = 18;
+let maxScore = 20;
+let percent = studentScore / maxScore * 100;
+
+console.log(percent);
