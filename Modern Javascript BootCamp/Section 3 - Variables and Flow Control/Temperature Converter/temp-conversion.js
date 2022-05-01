0d350f1c6e34fda3e12787c5ed463c6d8923bfc1
David Jiang <djiang86@binghamton.edu>
2022-05-01 10:13:25 -0400

    temperature converter

---
+++ Modern Javascript BootCamp/Section 3 - Variables and Flow Control/Temperature Converter/temp-conversion.js
@@ -0,0 +1,13 @@
+let fahrenheit = 32;
+
+// calculate celsius and store in celsius variable
+// print that value
+
+let celsius = (fahrenheit - 32) * 5 / 9;
+console.log(celsius);
+
+// calculate kelvin value and store in variables
+// print that value
+
+let kelvin = (fahrenheit + 459.67) * 5 / 9;
+console.log(kelvin);
