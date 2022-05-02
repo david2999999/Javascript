8bee6e86598a17090460685e08bf4bf723a17286
David Jiang <djiang86@binghamton.edu>
2022-05-02 13:29:36 -0400

    if statements

---
+++ Modern Javascript BootCamp/Section 3 - Variables and Flow Control/If Statements/boolean.js
@@ -0,0 +1,19 @@
+let temp = 32;
+
+if (temp <= 32) {
+    console.log('It is freezing outside');
+}
+
+if (temp >= 100) {
+    console.log('It is way too hot outside');
+}
+
+let age = 65;
+
+if (age <= 7) {
+    console.log('You will get a child discount!');
+}
+
+if (age >= 65) {
+    console.log('You will get a senior discount!');
+}
