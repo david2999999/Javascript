c8c753e810e9c7408a915767f919148969677c95
David Jiang <djiang86@binghamton.edu>
2022-05-05 17:33:51 -0400

    scope part 2

---
+++ Modern Javascript BootCamp/Section 3 - Variables and Flow Control/Variable Scope/scope-2.js
@@ -0,0 +1,15 @@
+
+let name = 'Andrew';
+
+if (true) {
+    let name = 'Mike';
+
+    if (true) {
+        name = 'Jen';
+        console.log(name);
+    }
+}
+
+if (true) {
+    console.log(name);
+}
