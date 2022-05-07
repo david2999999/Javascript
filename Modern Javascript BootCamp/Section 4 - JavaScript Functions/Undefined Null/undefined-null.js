c97bf3764258dc3b54109844426e1512004ef8cf
David Jiang <djiang86@binghamton.edu>
2022-05-07 09:09:45 -0400

    undefined and null

---
+++ Modern Javascript BootCamp/Section 4 - JavaScript Functions/Undefined Null/undefined-null.js
@@ -0,0 +1,25 @@
+// Undefined for variable
+let name;
+
+if (name === undefined) {
+    console.log('Please provide a name');
+} else {
+    console.log(name);
+}
+
+// Undefined for function arguments
+// Undefined as function return default value
+let square = function (num) {
+    console.log(num);
+};
+
+let result = square();
+
+console.log(result);
+
+// Null as assigned value
+let age = 27;
+
+age = null;
+
+console.log(age);
